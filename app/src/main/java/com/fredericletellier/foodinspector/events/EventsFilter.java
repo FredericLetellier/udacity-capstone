9ebf2753404b67fcafe3da027c3e234c1e7284c9
Frédéric Letellier <frederic.letellier.pro@gmail.com>
2016-10-27 15:27:49 +0200

    feat: Add more feature for EventsFilter

--- app/src/main/java/com/fredericletellier/foodinspector/events/EventsFilter.java
+++ app/src/main/java/com/fredericletellier/foodinspector/events/EventsFilter.java
@@ -18,17 +18,32 @@
 
 package com.fredericletellier.foodinspector.events;
 
+import android.os.Bundle;
+
+import com.fredericletellier.foodinspector.BuildConfig;
+
 public class EventsFilter {
 
-    private EventsFilterType mEventsFilterType;
+    public final static String KEY_EVENTS_FILTER = BuildConfig.APPLICATION_ID + "EVENTS_FILTER";
+    private EventsFilterType eventsFilterType = EventsFilterType.ALL_EVENTS;
+    private Bundle filterExtras;
 
-    protected EventsFilter(EventsFilterType eventsFilterType) {
-        this.mEventsFilterType = eventsFilterType;
+    protected EventsFilter(Bundle extras) {
+        this.filterExtras = extras;
+        this.eventsFilterType = (EventsFilterType) extras.getSerializable(KEY_EVENTS_FILTER);
+    }
+
+    public static EventsFilter from(EventsFilterType eventsFilterType){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(KEY_EVENTS_FILTER, eventsFilterType);
+        return new EventsFilter(bundle);
     }
 
     public EventsFilterType getEventsFilterType() {
-        return mEventsFilterType;
+        return eventsFilterType;
     }
 
+    public Bundle getFilterExtras() {
+        return filterExtras;
+    }
 }
-
